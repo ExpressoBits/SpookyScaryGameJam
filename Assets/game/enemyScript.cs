7de9fe8e47c7d0e3bdd3d0bc1b43d6a72ef81ab4
FloatingBanana <thalesmaiademoraes13@gmail.com>
2020-10-29 15:18:19 -0300

    O inimigo agora segue e ataca o jogador

--- Assets/game/enemyScript.cs
+++ Assets/game/enemyScript.cs
@@ -4,21 +4,55 @@ using UnityEngine;
 
 public class enemyScript : MonoBehaviour, IEntityTakeDamage
 {
-    float life = 100.0f;
-    float knockback = 0.0f;
-    int knockbackDir = 1;
+    Rigidbody2D rb;
+    Collider2D col;
+    Collider2D playerCol;
+
+    public float life = 100.0f;
+
+
+    Vector3 dir;
+    float speed = 2.5f;
+    bool isWalking = false;
+
+    float sightDistance = 7.0f;
+    float attackDamage = 10.0f;
+    float attackKnockback = 1.0f;
+
+    float waitForMovement = 0.0f;
 
     void Start() {
-        
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+        playerCol = GameObject.Find("player").GetComponent<Collider2D>();
     }
 
     void Update() {
-        
+        if (waitForMovement > 0.0f)
+            waitForMovement -= Time.deltaTime;
+
+        isWalking = false;
+        if (Vector2.Distance(playerCol.transform.position, transform.position) <= sightDistance && waitForMovement <= 0.0f) {
+            isWalking = true;
+            
+            dir = (playerCol.transform.position - transform.position).normalized;
+            transform.position += dir * speed * Time.deltaTime;
+        }
     }
 
-    public void takeDamage(float damage, float knock, int dealerDir) {
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider == playerCol) {
+            IEntityTakeDamage script = playerCol.GetComponent<IEntityTakeDamage>();
+            script.takeDamage(attackDamage, attackKnockback, dir);
+
+            waitForMovement = 0.7f;
+        }
+    }
+
+    public void takeDamage(float damage, float knockback, Vector3 dealerDir) {
         life -= damage;
-        knockback = knock;
-        knockbackDir = dealerDir;
+        waitForMovement = 0.5f;
+        
+        transform.position += dealerDir * knockback;
     }
 }
